08e93cba432f305488c209fe6446ed7ae4a1375a
Rachel_Nat <6791leon@gmail.com>
2024-12-12 11:54:49 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,32 @@
 #define N 5
 #define M 6
 
+// Функция для подсчёта положительных чисел
+int count_positive(int array[N][M], int rows, int cols) {
+    int count = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (array[i][j] > 0) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+// Функция для подсчёта отрицательных чисел
+int count_negative(int array[N][M], int rows, int cols) {
+    int count = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (array[i][j] < 0) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 int main() {
     int array[N][M] = {
         {1, -3, 4, -2, 6, -7},
@@ -12,16 +38,8 @@ int main() {
         {-26, 27, 28, -29, 30, -31}
     };
 
-    int pos_count = 0, neg_count = 0;
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (array[i][j] > 0)
-                pos_count++;
-            else if (array[i][j] < 0)
-                neg_count++;
-        }
-    }
+    int pos_count = count_positive(array, N, M);
+    int neg_count = count_negative(array, N, M);
 
     printf("Positive count: %d\n", pos_count);
     printf("Negative count: %d\n", neg_count);
